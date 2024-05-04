c4b31711acefb27aab340ae3bed058877e2bcbab
Andrey Sats <a.sats@a1qa.com>
2024-05-04 14:53:57 +0400

    Create a project with two tests

---
+++ AzureDevOpsProj/TestCases.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsProj
+{
+    [TestFixture]
+    public class TestCases
+    {
+        [Test]
+        [Category("PassedTest")]
+        public void TestToPass()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        [Category("FailedTest")]
+        public void TestToFail()
+        {
+            Assert.Fail();
+        }
+    }
+}
